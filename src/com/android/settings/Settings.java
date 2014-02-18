SlimRoms/packages_apps_Settings

Privacy Guard Slim version (2/2)
Re-implements slims ehanced privacy guard from 4.3
and adapt it to 4.4 changes

Change-Id: Ie87a72b4212e5a8063c1dfbf7fd759f3a14a0d12

src/com/android/settings/Settings.java
@@ -1083,6 +1083,7 @@ public class Settings extends PreferenceActivity
     public static class ApplicationSettingsActivity extends Settings { /* empty */ }
     public static class ManageApplicationsActivity extends Settings { /* empty */ }
     public static class AppOpsSummaryActivity extends Settings { /* empty */ }
+    public static class AppOpsDetailsActivity extends Settings { /* empty */ }
     public static class StorageUseActivity extends Settings { /* empty */ }
     public static class DevelopmentSettingsActivity extends Settings { /* empty */ }
     public static class AccessibilitySettingsActivity extends Settings { /* empty */ }

kasperhettinga/android_packages_apps_Settings-1

src/com/android/settings/Settings.java
@@ -1137,15 +1137,7 @@ public class Settings extends PreferenceActivity
     public static class DeviceInfoSettingsActivity extends Settings { /* empty */ }
     public static class ApplicationSettingsActivity extends Settings { /* empty */ }
     public static class ManageApplicationsActivity extends Settings { /* empty */ }
-    public static class AppOpsSummaryActivity extends Settings {
-        @Override
-        public boolean isValidFragment(String className) {
-            if (AppOpsSummary.class.getName().equals(className)) {
-                return true;
-            }
-            return super.isValidFragment(className);
-        }
-    }
+    public static class AppOpsSummaryActivity extends Settings { /* empty */ }
     public static class StorageUseActivity extends Settings { /* empty */ }
     public static class DevelopmentSettingsActivity extends Settings { /* empty */ }
     public static class AccessibilitySettingsActivity extends Settings { /* empty */ }